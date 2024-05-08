8314d5846ba06cd3b4c744d811d1ed0945e30006
daniellcferreira <146852579+daniellcferreira@users.noreply.github.com>
2024-05-08 12:20:18 -0300

    create lesson tipos-variaveis

---
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -0,0 +1,19 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+
+        // Tipos primitivos variaveis
+
+        byte idade = 110;
+        short ano = 2021;
+        int cep = 21070333;
+        long cpf = 987654321;
+        float pi = 3.14F;
+        double salario = 1275.33;
+
+
+        // Constantes
+
+        final double VALOR_PI = 3.14;
+
+    }
+}
