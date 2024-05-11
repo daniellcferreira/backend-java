e3dcccc55b12808fb68bc57e4a708ff0ee9f9436
daniellcferreira <146852579+daniellcferreira@users.noreply.github.com>
2024-05-11 17:33:46 -0300

    create lesson controle-fluxo

---
+++ controle-fluxo/src/PlanoOperadora.java
@@ -0,0 +1,25 @@
+public class PlanoOperadora {
+    
+    public static void main(String[] args) {
+        
+        String plano = "M";
+
+        switch (plano) {
+            case "T": {
+                System.out.println("5Gb Youtube");
+                break;
+            }
+                
+            case "M": {
+                System.out.println("Whats e Intagram gratis");
+                break;
+            }
+
+            case "B": {
+                System.out.println("100 minutos ligação");
+                break;
+            }
+        }
+
+    }
+}
