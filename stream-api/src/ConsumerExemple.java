fba9e8324c29690b86950b1c3a5fc9d40d6cc950
daniellcferreira <146852579+daniellcferreira@users.noreply.github.com>
2024-05-20 11:10:15 -0300

    create lesson strem-api

---
+++ stream-api/src/ConsumerExemple.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class ConsumerExemple {
+    
+    public static void main(String[] args) {
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8);
+
+        Consumer<Integer> imprimirNumeroPar = numero -> {
+            if (numero % 2 == 0) {
+                System.out.println(numero);   
+            }
+        };
+
+        numeros.forEach(n -> {
+            if (n % 2 == 0) {
+                System.out.println(n);     
+            }
+        });
+
+    }
+}
