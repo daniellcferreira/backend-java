7be7af4cbcebbb28cd8faeb150750290f17281b4
daniellcferreira <146852579+daniellcferreira@users.noreply.github.com>
2024-06-04 15:08:50 -0300

    create lesson spring-web-security

---
+++ spring-boot/security-database/src/test/java/dio/security_database/SecurityDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.security_database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
