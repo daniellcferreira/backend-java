42f3bcaac6241e54c5b8855709e1ef2f3048186c
daniellcferreira <146852579+daniellcferreira@users.noreply.github.com>
2024-11-23 10:19:44 -0300

    update lesson

--- spring-boot/spring-kafka/microsservico-kafka/src/main/java/dio/microsservico_kafka/service/SalvarPedidoService.java
+++ spring-boot/spring-kafka/microsservico-kafka/src/main/java/dio/microsservico_kafka/service/SalvarPedidoService.java
@@ -1,11 +1,41 @@
 package dio.microsservico_kafka.service;
 
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import dio.microsservico_kafka.data.PedidoData;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Service
 public class SalvarPedidoService {
 
-    private void executar() {
+     @KafkaListener(topics = "SalvarPedido", groupId = "MicrosservicoSalvaPedido")
+    private void executar(ConsumerRecord<String, String> record) {
+
+        log.info("Chave = {}", record.key());
+        log.info("Cabecalho = {}", record.headers());
+        log.info("Particao = {}", record.partition());
+
+        String strDados = record.value();
+
+        ObjectMapper mapper = new ObjectMapper();
+        PedidoData pedido;
+
+        try {
+            pedido = mapper.readValue(strDados, PedidoData.class);
+        } catch (JsonProcessingException ex) {
+            log.error("Falha converter evento [dado={}}]", strDados, ex);
+            return;
+        }
+
+        log.info("Evento Recebido = {}", pedido);
+
         
     }
+
 }
