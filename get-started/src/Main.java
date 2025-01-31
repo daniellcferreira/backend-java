4f6d865c6189ef973678bc6dbfb64d25d8d88cd7
Daniel Ferreira <dcfer2017@gmail.com>
2025-01-31 10:12:07 -0300

    update lesson

--- get-started/src/Main.java
+++ get-started/src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello World");
+
+    var male = new Person("John");
+    male.setAge(25);
+
+    var female = new Person("Jane");
+    female.setAge(22);
+
+    System.out.println("Male name: " + male.getName() + " age: " + male.getAge());
+    System.out.println("Female name: " + female.getName() + " age: " + female.getAge());
   }
 
 }
---
+++ get-started/src/Person.java
@@ -0,0 +1,21 @@
+public class Person {
+
+  private final String name;
+  private int age;
+
+  public Person(String name) {
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+}
